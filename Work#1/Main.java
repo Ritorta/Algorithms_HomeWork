b119da1f0e987de289f91f6be81f4845db8310ec
Ritorta <Meddesu@yandex.ru>
2023-12-13 15:06:22 +0500

    Fix format and headsort create

---
+++ Work#1/Main.java
@@ -0,0 +1,72 @@
+// Урок 2. Структуры данных. Массивы. Алгоритмы массивов.
+
+// Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
+
+import java.util.Random;
+
+public class Main 
+{
+    void compareElementRoot(int array[], int n, int i)
+    {
+        int root = i; // наибольший элемент в корне
+        int l = 2 * i + 1; // левый элемент в корне
+        int r = 2 * i + 2; // правый элемент в корне
+
+        if(l < n && array[l] > array[root]) // Если левый элемет больше корня
+        {
+            root = l;
+        }
+
+        if(r < n && array[r] > array[root]) // Если правый элемент больще корня
+        {
+            root = r;
+        }
+
+        if(root != i) // Если корень не самый большой элемент
+        {
+            int temp = array[i];
+            array[i] = array[root];
+            array[root] = temp;
+        }
+
+        compareElementRoot(array, n, root); // Рекурсия в двойную кучу
+
+    }
+    
+    public void heapSort(int array[])
+    {
+        int n = array.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) // Постороение кучи 
+        {
+            compareElementRoot(array, n, i);
+        }
+
+        for(int i = n - 1; i >= 0; i--) // Извлечение элементов кучи
+        {
+            // Переставляем отсортированный элемент массива в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            compareElementRoot(array, i, 0); // начинаем сортировку оставшегося массива
+
+        }
+
+    }
+
+
+    public static void main(String[] args) 
+    {
+        Random rand = new Random();
+        int array[] = new int[30];
+        for (int i = 0; i < array.length; i++) 
+        {
+            array[i] = rand.nextInt(100);
+            System.out.println(array[i] + ' ');    
+        } 
+        
+        
+    }
+
+}
