e2f9b4762768f94cbd8b523608d476d1310a647c
Ritorta <Meddesu@yandex.ru>
2023-12-28 16:22:35 +0500

    30^36

---
+++ Work#3/HashTable.java
@@ -0,0 +1,54 @@
+public class HashTable<K, V> 
+{
+    private static final int INIT_BASCET_COUNT = 16;
+    
+    private Basket[] baskets;
+    
+    public HashTable(int initSize) 
+    {
+        this.baskets = (Basket[])new Object[initSize];
+    }
+
+    public HashTable()
+    {
+        this(INIT_BASCET_COUNT);
+    }
+
+    private int calcilateBasketIndex(K key)
+    {
+        return key.hashCode() % baskets.length;
+    }
+
+    private class Entity
+    {
+        private K key;
+        private V value;
+    }
+
+    private class Basket
+    {
+        private Node head;
+
+        private class Node
+        {
+            private Node next;
+            private Entity value;
+        }
+
+        public V get(K key)
+        {
+            Node node = head;
+
+            while(node != null)
+            {
+                if(node.value.key.equals(key))
+                {
+                    return node.value.value;
+                }
+                node = node.next;
+            }
+            return null;
+        }
+    }
+    
+}
