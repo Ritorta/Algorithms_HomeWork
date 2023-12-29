cdf04929299f758977d90001b6204d6e60828832
Ritorta <Meddesu@yandex.ru>
2023-12-29 09:28:19 +0500

    57^50

--- Work#3/HashTable.java
+++ Work#3/HashTable.java
@@ -35,6 +35,28 @@ public class HashTable<K, V>
         private V value;
     }
 
+    public boolean put(K key, V value)
+    {
+        int index = calcilateBasketIndex(key);
+        Basket basket = baskets[index];
+        if(basket == null)
+        {
+            basket = new Basket();
+            baskets[index] = basket;
+        }
+        Entity entity = new Entity();
+        entity.key = key;
+        entity.value = value;
+        return basket.add(entity);
+    }
+
+    public boolean remove(K key)
+    {
+        int index = calcilateBasketIndex(key);
+        Basket basket = baskets[index];
+        return basket.remove(key);
+    }
+
     private class Basket
     {
         private Node head;
@@ -56,6 +78,62 @@ public class HashTable<K, V>
                 node = node.next;
             }
             return null;
+        }
+
+        public boolean remove(K key)
+        {
+            if(head !=null)
+            {
+                if(head.value.key.equals(key))
+                {
+                    head = head.next;
+                }
+                else
+                {
+                    Node node = head;
+                    while(node != null)
+                    {
+                        if(node.next.value.key.equals(key))
+                        {
+                            node.next = node.next.next;   
+                            return true;     
+                        }       
+                        node = node.next;
+                    }
+                }
+            }
+            return false;
+        }
+        
+        public boolean add(Entity entity)
+        {
+            Node node = new Node();
+            node.value = entity;
+            if(head != null)
+            {
+                Node current = head;
+                while(true)
+                {
+                    if(current.value.key.equals(entity.key))
+                    {
+                        return false;
+                    }
+                    if(current.next == null)
+                    {
+                        current.next = node;
+                        return true;
+                    }
+                    else
+                    {
+                        current = current.next;
+                    }
+                }
+            }
+            else
+            {
+                head = node;
+                return true;
+            }
         }
     }
     
